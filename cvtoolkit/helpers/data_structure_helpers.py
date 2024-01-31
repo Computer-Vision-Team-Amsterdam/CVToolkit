07285da233113d195bdb31ee6ed40c80d0cc3319
Sebastian Davrieux <s.davrieux@amsterdam.nl>
2024-01-31 10:55:47 +0100

    Added example to data_structure_helpers

--- cvtoolkit/helpers/data_structure_helpers.py
+++ cvtoolkit/helpers/data_structure_helpers.py
@@ -14,5 +14,27 @@ def flatten_dict_to_list_of_dicts(dictionary: Dict[str, list]) -> list:
     -------
     list
         A flattened list of dictionaries.
+
+    Example
+    -------
+    input_dict = {
+        'group1': [
+            {'gender': 'Male', 'age': 25, 'skin_tone': 'Light'},
+            {'gender': 'Female', 'age': 30, 'skin_tone': 'Light'}
+        ],
+        'group2': [
+            {'gender': 'Male', 'age': 22, 'skin_tone': 'Medium'},
+            {'gender': 'Female', 'age': 28, 'skin_tone': 'Dark'}
+        ]
+    }
+
+    result_list = flatten_dict_to_list_of_dicts(input_dict)
+
+    result_list = [
+        {'gender': 'Male', 'age': 25, 'skin_tone': 'Light'},
+        {'gender': 'Female', 'age': 30, 'skin_tone': 'Light'},
+        {'gender': 'Male', 'age': 22, 'skin_tone': 'Medium'},
+        {'gender': 'Female', 'age': 28, 'skin_tone': 'Dark'}
+    ]
     """
     return [item for sublist in dictionary.values() for item in sublist]
